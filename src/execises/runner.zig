579521e3766ce0bf9f906df2f37d8e56462105cc
karim-w <karim.wael@gmail.com>
2023-06-30 06:28:29 +0400

    feat: added ex 15

--- src/execises/runner.zig
+++ src/execises/runner.zig
@@ -10,6 +10,7 @@ const Ex11 = @import("./011.zig");
 const Ex12 = @import("./012.zig");
 const Ex13 = @import("./013.zig");
 const Ex14 = @import("./014.zig");
+const Ex15 = @import("./015.zig");
 pub fn ExerciseRunner() void {
     Ex3.Ex3();
     Ex4.Ex();
@@ -23,4 +24,5 @@ pub fn ExerciseRunner() void {
     Ex12.Ex();
     Ex13.Ex();
     Ex14.Ex();
+    Ex15.Ex();
 }
